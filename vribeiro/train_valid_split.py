79e99386cc863e6a86de1a319eccca222614ea99
Vinicius Ribeiro <vinicius.ribeiro1@gmail.com>
2020-07-04 20:54:26 -0300

    Stratify train and valid split by target

--- vribeiro/train_valid_split.py
+++ vribeiro/train_valid_split.py
@@ -4,39 +4,35 @@ import pandas as pd
 from sklearn.model_selection import StratifiedKFold
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-DATA_DIR = os.path.join(os.path.dirname(BASE_DIR), "data")
-
+DATA_DIR = os.path.join(BASE_DIR, "data")
 TRAIN_FPATH = os.path.join(DATA_DIR, "train.csv")
 
-df_train = pd.read_csv(TRAIN_FPATH)
-df_patients = df_train.groupby("patient_id").agg({"target": lambda series: series.max()}).reset_index()
+df = pd.read_csv(TRAIN_FPATH)
 
 skf = StratifiedKFold()
-for i, (train, valid) in enumerate(skf.split(df_patients.patient_id, df_patients.target)):
+for i, (train, valid) in enumerate(skf.split(df, df.target)):
     print(f"Fold {i + 1}:")
 
     fold_dir = os.path.join(DATA_DIR, f"fold_{i + 1}")
     if not os.path.isdir(fold_dir):
         os.makedirs(fold_dir)
 
-    train_patients = df_patients.iloc[train].patient_id
-    df_fold_train = df_train[df_train.patient_id.isin(train_patients)]
-    train_counts = train_fold.target.value_counts()
+    df_train = df.iloc[train]
+    train_counts = df_train.target.value_counts()
 
     neg = train_counts[0]
     pos = train_counts[1]
     print(f"train: {pos / (pos + neg)}")
 
-    train_fold.to_csv(os.path.join(fold_dir, f"train.csv"), index=False)
+    df_train.to_csv(os.path.join(fold_dir, f"train.csv"), index=False)
 
-    valid_patients = df_patients.iloc[valid].patient_id
-    df_fold_valid = df_train[df_train.patient_id.isin(valid_patients)]
-    valid_counts = valid_fold.target.value_counts()
+    df_valid = df.iloc[valid]
+    valid_counts = df_valid.target.value_counts()
 
     neg = valid_counts[0]
     pos = valid_counts[1]
     print(f"valid: {pos / (pos + neg)}")
 
-    valid_fold.to_csv(os.path.join(fold_dir, f"valid.csv"), index=False)
+    df_valid.to_csv(os.path.join(fold_dir, f"valid.csv"), index=False)
 
     print("-------")
